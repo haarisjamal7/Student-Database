75168c639c5da2070ce75b465ac49cac0adbb1e1
haarisjamal7 <116809905+haarisjamal7@users.noreply.github.com>
2024-05-16 01:25:03 +0530

    Add files via upload

---
+++ cwh/01_basics.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    <div class ="container">
+        This is My First PHP Website
+        <?php
+        echo "Hello World using PHP";
+        ?>
+        <?php
+        echo "Hello World again using PHP";
+        ?>
+    </div> 
+</body>
+</html>
